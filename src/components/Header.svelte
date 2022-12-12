8f6ef1eb0cbddfd6fdc8cbb3e865405f04d35093
Cleyson Barbosa <cleysonn@hotmail.com.br>
2022-12-12 07:15:26 -0300

    FEAT: App header

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+  const doSomething = () => {
+    alert("Click on button")
+  }
+</script>
+
+<style>
+  header {
+    position: sticky;
+    display: flex;
+    top: 0;
+    width: 100%;
+    background-color: white;
+    margin-top: 112px;
+    padding: 0 32px;
+  }
+</style>
+
+<header>
+  <h1>BTracker</h1>
+  <button on:click={doSomething}>Log in</button>
+</header>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import './app.css'
-import App from './App.svelte'
+import "./index.css"
+import "./app.css"
+
+import App from "./App.svelte"
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.getElementById("app"),
 })
 
 export default app
